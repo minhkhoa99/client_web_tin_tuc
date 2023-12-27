c3f7eb4967f17688996303e0c2769217cb25d3f7
minhkhoa99 <110026964+minhkhoa99@users.noreply.github.com>
2023-12-27 21:34:48 +0700

    Revert "Update Home-Page"

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import "./App.css";
+import Registration from "./pages/Registration/Registration";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePages from "./pages/homePages/homePages";
-import Headers from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePages from "./pages/Login/homePages";
 function App() {
   return (
     <div className="App">
-      <Headers />
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-              <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePages />}></Route>
-        </Routes>
-      </BrowserRouter>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter>
+      
+      <Routes>
+      <Route path="/pages/home" element = {<HomePages/>}></Route>
+      <Route path="/user/register" element={  <Registration />}></Route>
+      </Routes>
     
+      </BrowserRouter>
 
-      <Footer/>
     </div>
   );
 }
