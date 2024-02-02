3b15610c72b303f9e13221095e0fb0f6db0d545e
thong1403 <110025148+thong1403@users.noreply.github.com>
2024-02-02 19:45:30 +0700

    Update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,107 +1,15 @@
-import { useState } from "react";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 import imageBackgroud from "../../image/backgroud.jpg"
 
 import "./footer.css";
+import Navbar from "../navbar/Navbar";
 
 const Footer = () => {
-  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
-  function toggleNavbar() {
-    setIsNavbarOpen(!isNavbarOpen);
-  }
+
   return (
     <div>
-      <div
-          className="row"
-          style={{
-            boxSizing: "border-box",
-            marginRight: "-5px",
-            marginLeft: "-5px",
-          }}
-        >
-          <div
-            className="col-xs-12"
-            style={{
-              boxSizing: "border-box",
-              position: "relative",
-              minHeight: "1px",
-              paddingRight: "5px",
-              paddingLeft: "5px",
-              cssFloat: "left",
-              width: "100%",
-            }}
-          >
-            <div id="MenuBarContainer" style={{ boxSizing: "border-box" }}>
-              <div id="TopMenuLeft" style={{ boxSizing: "border-box" }}>
-                <div
-                  id="ctl00_g_e803a2bb_79d8_4279_880c_15c8682f4c5c"
-                  style={{ boxSizing: "border-box" }}
-                >
-                  <nav className="navbar navbar-expand-lg bg-body-tertiary" id="nav-header">
-                    <div className="container-fluid">
-                      <button
-                        className="navbar-toggler"
-                        type="button"
-                        onClick={toggleNavbar}
-                        aria-label="Toggle navigation"
-                      >
-                        <span className="navbar-toggler-icon"></span>
-                      </button>
-                      <div
-                        className={`collapse navbar-collapse ${
-                          isNavbarOpen ? "show" : ""
-                        }`}
-                        id="navbarNav"
-                      >
-                        <ul class="navbar-nav container-menu-nav-items m-auto">
-                          <li class="nav-item mx-3">
-                            <a
-                              class="nav-link active"
-                              aria-current="page"
-                              href="/"
-                            >
-                              Trang Chủ
-                            </a>
-                          </li>
-                          <li class="nav-item mx-3">
-                            <a class="nav-link active" href="/introduce">
-                              Giới Thiệu Chung
-                            </a>
-                          </li>
-                          <li class="nav-item mx-3">
-                            <a class="nav-link active" href="/promotional-media">
-                              Truyền thông cổ động
-                            </a>
-                          </li>
-                          <li class="nav-item mx-3">
-                            <a class="nav-link active" href="/public-art">
-                              Nghệ thuật quần chúng
-                            </a>
-                          </li>
-                          <li class="nav-item mx-3">
-                            <a class="nav-link active" href="/news">
-                              Tin Tức Sự Kiện
-                            </a>
-                          </li>
-                          <NavDropdown title="Lưu động" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/propagate" className="nav-dropdown mt-3 pb-2">
-                              Tuyên truyền lưu động
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="/movies" className="nav-dropdown mt-3">
-                              Chiếu phim lưu động
-                            </NavDropdown.Item>
-                            
-                          </NavDropdown>
-                        </ul>
-                      </div>
-                    </div>
-                  </nav>
-                </div>
-              </div>
-            </div>{" "}
-          </div>
-        </div>
+    <Navbar />
       <div className="container-footer">
         <img src={imageBackgroud} alt="" className="w-100" />
       </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,105 +1,170 @@
 import { useState } from "react";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Navbar = () => {
-    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
-    function toggleNavbar() {
-      setIsNavbarOpen(!isNavbarOpen);
-    }
-    return(
-        <div
-        className="row"
-        style={{
-          boxSizing: "border-box",
-          marginRight: "-5px",
-          marginLeft: "-5px",
-        }}
-      >
-        <div
-          className="col-xs-12"
-          style={{
-            boxSizing: "border-box",
-            position: "relative",
-            minHeight: "1px",
-            paddingRight: "5px",
-            paddingLeft: "5px",
-            cssFloat: "left",
-            width: "100%",
-          }}
-        >
-          <div id="MenuBarContainer" style={{ boxSizing: "border-box" }}>
-            <div id="TopMenuLeft" style={{ boxSizing: "border-box" }}>
-              <div
-                id="ctl00_g_e803a2bb_79d8_4279_880c_15c8682f4c5c"
-                style={{ boxSizing: "border-box" }}
+  function toggleNavbar() {
+    setIsNavbarOpen(!isNavbarOpen);
+  }
+  return (
+    <div id="navbarConatainerBackgroud">
+      <div className="row">
+        <div className="col-xs-12">
+          <div id="MenuBarContainer">
+            <div id="TopMenuLeft">
+              <nav
+                className="navbar navbar-expand-lg bg-body-tertiary"
+                id="nav-header"
               >
-                <nav className="navbar navbar-expand-lg bg-body-tertiary" id="nav-header">
-                  <div className="container-fluid">
-                    <button
-                      className="navbar-toggler"
-                      type="button"
-                      onClick={toggleNavbar}
-                      aria-label="Toggle navigation"
-                    >
-                      <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div
-                      className={`collapse navbar-collapse ${
-                        isNavbarOpen ? "show" : ""
-                      }`}
-                      id="navbarNav"
-                    >
-                      <ul className="navbar-nav container-menu-nav-items m-auto">
-                        <li className="nav-item mx-2">
-                          <a
-                            className="nav-link active p-0"
-                            aria-current="page"
-                            href="/"
-                          >
-                            Trang Chủ
-                          </a>
-                        </li>
-                        <li className="nav-item mx-2">
-                          <a className="nav-link active p-0" href="/introduce">
-                            Giới Thiệu Chung
-                          </a>
-                        </li>
-                        <li className="nav-item mx-2">
-                          <a className="nav-link active p-0" href="/promotional-media">
-                            Truyền thông cổ động
-                          </a>
-                        </li>
-                        <li className="nav-item mx-2">
-                          <a className="nav-link active p-0" href="/public-art">
-                            Nghệ thuật quần chúng
-                          </a>
-                        </li>
-                        <li className="nav-item mx-2">
-                          <a className="nav-link active p-0" href="/news">
-                            Tin Tức Sự Kiện
-                          </a>
-                        </li>
-                        <NavDropdown title="Lưu động" id="basic-nav-dropdown" className="p-0 mx-2">
-                          <NavDropdown.Item href="/propagate" className="nav-dropdown mt-3 pb-2">
-                            Tuyên truyền lưu động
-                          </NavDropdown.Item>
-                          <NavDropdown.Item href="/movies" className="nav-dropdown mt-3">
-                            Chiếu phim lưu động
-                          </NavDropdown.Item>
-                          
-                        </NavDropdown>
-                      </ul>
-                    </div>
-                  </div>
-                </nav>
-              </div>
-            </div>
-          </div>{" "}
-        </div>
-      </div>
-    )
-}
+                <div className="container-fluid">
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    onClick={toggleNavbar}
+                    aria-label="Toggle navigation"
+                  >
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div
+                    className={`collapse navbar-collapse ${
+                      isNavbarOpen ? "show" : ""
+                    }`}
+                    id="navbarNav"
+                  >
+                    <ul className="navbar-nav container-menu-nav-items m-auto">
+                      <li className="nav-item mx-2">
+                        <a
+                          className="nav-link active"
+                          aria-current="page"
+                          href="/"
+                        >
+                          Trang Chủ
+                        </a>
+                      </li>
+                      <NavDropdown
+                        title="Giới thiệu"
+                        id="basic-nav-dropdown"
+                        className="p-0 mx-2"
+                      >
+                        <NavDropdown.Item
+                          href="/introduce"
+                          className="nav-dropdown pb-2"
+                        >
+                          Giới thiệu chung
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/introduce"
+                          className="nav-dropdown"
+                        >
+                          Tổ chức bộ máy
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/introduce"
+                          className="nav-dropdown"
+                        >
+                          Sơ đồ trang
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                      <NavDropdown
+                        title="Nghiệp vụ văn hóa"
+                        id="basic-nav-dropdown"
+                        className="p-0 mx-2"
+                      >
+                        <NavDropdown.Item
+                          href="/promotional-media"
+                          className="nav-dropdown pb-2"
+                        >
+                          Tuyên truyền cổ động - Triển lãm
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/public-art"
+                          className="nav-dropdown"
+                        >
+                          TNghệ thuật quần chúng
+                        </NavDropdown.Item>
+                      </NavDropdown>
 
-export default Navbar
+                      <NavDropdown
+                        title="Tuyên truyền lưu động"
+                        id="basic-nav-dropdown"
+                        className="p-0 mx-2"
+                      >
+                        <NavDropdown.Item
+                          href="/propagate/"
+                          className="nav-dropdown pb-2"
+                        >
+                          Hoạt động tuyên truyền
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/propagate/"
+                          className="nav-dropdown"
+                        >
+                          Kịch bản tuyên truyền
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                      <li className="nav-item mx-2">
+                        <a
+                          className="nav-link active"
+                          aria-current="page"
+                          href="/movies/"
+                        >
+                          Nghiệp vụ điện ảnh
+                        </a>
+                      </li>
+                      <NavDropdown
+                        title="Thông tin nghiệp vụ"
+                        id="basic-nav-dropdown"
+                        className="p-0 mx-2"
+                      >
+                        <NavDropdown.Item
+                          href="/news"
+                          className="nav-dropdown pb-2"
+                        >
+                          Văn bản chỉ đạo - Điều hành
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/news"
+                          className="nav-dropdown"
+                        >
+                          Khẩu lệnh
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/news"
+                          className="nav-dropdown"
+                        >
+                          Bài phát thanh
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/news"
+                          className="nav-dropdown"
+                        >
+                          Bộ triển lãm
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/news"
+                          className="nav-dropdown"
+                        >
+                          Bộ tranh cổ động
+                        </NavDropdown.Item>
+                        <NavDropdown.Item
+                          href="/news"
+                          className="nav-dropdown"
+                        >
+                          Thông báo
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>{" "}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
